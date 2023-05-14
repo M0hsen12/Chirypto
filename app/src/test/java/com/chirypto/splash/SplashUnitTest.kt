3d0db9bad0454f8bb2165b8f4a93923c4231ebb1
Goodarzi <goudarzi1212@gmail.com>
2023-05-14 18:32:03 +0330

    refactoring the Unit test class names

---
+++ app/src/test/java/com/chirypto/splash/SplashUnitTest.kt
@@ -0,0 +1,33 @@
+package com.chirypto.splash
+
+class SplashUnitTest {
+    /*
+    1.testing update state
+    2.testing error connection state
+    3.testing normal state
+    4.testing the signedUser (maybe after the sign up)
+
+     */
+
+//    @Test
+//    fun gettingTheUpdateApplicationState() {
+//        val viewModel = SplashViewModel("")
+//        assertEquals(SplashState.UpdateDialog, viewModel.displayUpdateDialog())
+//    }   @Test
+//    fun gettingSplashState() {
+//        val viewModel = SplashViewModel("")
+//        assertEquals(SplashState.Normal, viewModel.displaySplash())
+//    }
+//    @Test
+//    fun gettingErrorNetworkConnectivity() {
+//        val viewModel = SplashViewModel("")
+//        assertEquals(SplashState.NetworkConnectivityError, viewModel.displayNetworkConnectivityError())
+//    }
+//
+//    @Test
+//    fun gettingSplashScreenState(){
+//        val viewModel = SplashViewModel("")
+//        assertEquals(SplashState.Normal, viewModel.gettingTheSplashState(BuildConfig.VERSION_CODE))
+//
+//    }
+}
